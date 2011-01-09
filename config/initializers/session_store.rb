434922632f1d81c57824349a81d78e0b34aa02f1
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-01-09 17:31:03 -0500

    updated session store cookie name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Uptimetry::Application.config.session_store :cookie_store, :key => '_vigilant_session'
+Uptimetry::Application.config.session_store :cookie_store, :key => '_uptimetry_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
