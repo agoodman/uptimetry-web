619db87191819c838096f155fcc40f51b60e08e8
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-01-09 17:26:08 -0500

    oops

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::SessionsController < Api::ApiController
     @user = ::User.authenticate(params[:session][:email], params[:session][:password])
     respond_to do |format|
       if @user
-        format.json { render :json => @user.to_json(:only => [:user_id, :remember_token])}
+        format.json { render :json => @user.to_json(:only => [:id, :remember_token])}
       else
         format.json { render :json => { :errors => [ 'Email and password does not match.' ] }, :status => :unprocessible_entity }
       end
