dad0b930db1974099cf12e6a993cc76833add183
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-01-11 00:04:16 -0500

    added dependent destroy for sites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   
-  has_many :sites
+  has_many :sites, :dependent => :destroy
 
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
   validates_presence_of :first_name, :last_name
