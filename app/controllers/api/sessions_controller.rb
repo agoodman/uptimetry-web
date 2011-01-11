ca08a9628ee17a05d3a80d65605ba72cadec5ee6
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-01-11 02:46:04 -0500

    set explicit nil check on user

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,11 +3,11 @@ class Api::SessionsController < Api::ApiController
   def create
     @user = ::User.authenticate(params[:session][:email], params[:session][:password])
     respond_to do |format|
-      if @user
+      if @user.nil?
+        format.json { render :json => { :errors => [ 'Email and password does not match.' ] }, :status => :unprocessable_entity }
+      else
         sign_in(@user)
         format.json { render :json => { :session => { :user_id => @user.id, :remember_token => @user.remember_token } } }
-      else
-        format.json { render :json => { :errors => [ 'Email and password does not match.' ] }, :status => :unprocessable_entity }
       end
     end
   end
