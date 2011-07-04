94f06fc45a9a478224fa738e8fb67131ae64652c
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-07-04 05:31:59 -0400

    updated with new api token

--- config/initializers/enrollmint.rb
+++ config/initializers/enrollmint.rb
@@ -3,6 +3,6 @@ require 'enrollmint'
 if Rails.env!='test' then
   EnrollMint.configure do |config|
     config.sandbox = true
-    config.api_token = '806ce0cdd2'
+    config.api_token = 'd04d10eb3a'
   end
 end
